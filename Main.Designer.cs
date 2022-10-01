c87b18787db1690f8062d514f7e65f690670c22e
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-10-01 19:15:38 -0400

    Way to many changes

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,29 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // mainPanel
             // 
             this.mainPanel.Location = new System.Drawing.Point(12, 12);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(430, 510);
+            this.mainPanel.Size = new System.Drawing.Size(420, 502);
             this.mainPanel.TabIndex = 1;
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Nitroless";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TrayIcon_DoubleClick);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 534);
+            this.ClientSize = new System.Drawing.Size(444, 526);
             this.Controls.Add(this.mainPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "Nitroless";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_Closing);
             this.Load += new System.EventHandler(this.Main_Load);
+            this.Resize += new System.EventHandler(this.Main_Resize);
             this.ResumeLayout(false);
 
         }
@@ -58,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.NotifyIcon trayIcon;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -19,9 +19,83 @@ namespace Nitroless
         public Main()
         {
             InitializeComponent();
+            if (!File.Exists("repos.json"))
+            {
+                File.Create("repos.json").Close();
+                File.AppendAllText("repos.json", "[]");
+            }
+            this.BackColor = Color.FromArgb(60, 63, 65);
+            // this.TopMost = true;
         }
 
         private void Main_Load(object sender, EventArgs e)
+        {
+            TrayMenu();
+            LowerRight();
+            LoadPanel();
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                this.Hide();
+            }
+        }
+
+        private void Main_Closing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void TrayMenu()
+        {
+            trayIcon.ContextMenuStrip = new ContextMenuStrip();
+            trayIcon.ContextMenuStrip.Items.Add("Show", null, Main_Show);
+            trayIcon.ContextMenuStrip.Items.Add("Hide", null, Main_Hide);
+            trayIcon.ContextMenuStrip.Items.Add("Add Repo", null, Main_AddRepo);
+            trayIcon.ContextMenuStrip.Items.Add("Delete Repo", null, Main_DeleteRepo);
+            trayIcon.ContextMenuStrip.Items.Add("Exit", null, Main_Close);
+        }
+        private void Main_Show(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+        }
+        private void Main_Hide(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        private void Main_AddRepo(object sender, EventArgs e)
+        {
+        }
+
+        private void Main_DeleteRepo(object sender, EventArgs e)
+        {
+        }
+
+        private void Main_Close(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void LowerRight()
+        {
+            Screen rightmost = Screen.AllScreens[0];
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen.WorkingArea.Right > rightmost.WorkingArea.Right)
+                    rightmost = screen;
+            }
+
+            this.Left = rightmost.WorkingArea.Right - this.Width;
+            this.Top = rightmost.WorkingArea.Bottom - this.Height;
+        }
+
+        private void LoadPanel()
         {
             string reposJson = File.ReadAllText("repos.json");
             dynamic reposArray = JsonConvert.DeserializeObject(reposJson);
@@ -29,6 +103,7 @@ namespace Nitroless
             {
                 int x = 0;
                 int y = 0;
+                int t = 0;
 
                 for (int a = 0; a < reposArray.Count; a++)
                 {
@@ -50,6 +125,7 @@ namespace Nitroless
                     repoLabel.AutoSize = true;
                     repoLabel.Font = new Font(repoLabel.Font.Name, 18);
                     repoLabel.Padding = new Padding(0);
+                    repoLabel.ForeColor = Color.FromArgb(220, 220, 220);
                     mainPanel.Controls.Add(repoLabel);
 
                     x = 0;
@@ -57,23 +133,44 @@ namespace Nitroless
 
                     for (int b = 0; b < reposJsonResponse.emotes.Count; b++)
                     {
+                        string emoteImageUrl = reposArray[a] + reposJsonResponse.path + "/" + reposJsonResponse.emotes[b].name + "." + reposJsonResponse.emotes[b].type;
                         PictureBox emotePictureBox = new PictureBox();
-                        emotePictureBox.ImageLocation = reposArray[a] + reposJsonResponse.path + "/" + reposJsonResponse.emotes[b].name + "." + reposJsonResponse.emotes[b].type;
+                        emotePictureBox.ImageLocation = emoteImageUrl;
                         emotePictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
                         emotePictureBox.Size = new Size(35, 35);
+                        emotePictureBox.MouseClick += (sender, EventArgs) => { EmotePictureBox_Click(emoteImageUrl); };
                         emotePictureBox.Cursor = Cursors.Hand;
                         emotePictureBox.Location = new Point(x, y);
                         mainPanel.Controls.Add(emotePictureBox);
                         x += 40;
+                        t += 1;
+                        if (t == 8)
+                        {
+                            x = 0;
+                            y += 40;
+                            t = 0;
+                        }
+                        if (b == (reposJsonResponse.emotes.Count - 1))
+                        {
+                            x = 0;
+                            y += 80;
+                            t = 0;
+                        }
                     }
-
-                    x = 0;
-                    y += 80;
                 }
-
-                x = 0;
-                y = 0;
             }
+        }
+
+        private void EmotePictureBox_Click(string pictureurl)
+        {
+            Clipboard.SetText(pictureurl);
+        }
+
+        private void TrayIcon_DoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
         }
     }
 }
